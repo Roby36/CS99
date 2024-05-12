e9e7b1a8cbe89114b7c96a4bf91d4d5acc522de6
Roby <roberto.brera.24@dartmouth.edu>
2024-05-12 19:11:23 -0400

    hom_classes struct for each vertex & heuristic set to zero for exploring all classes

--- vertex.h
+++ vertex.h
@@ -2,21 +2,23 @@
 #pragma once 
 
 #include <stdbool.h>
-#include "Lval_list.h"
+#include "hom_classes.h"
 
 struct minheap_node; // forward declaration of min_heap node
 
 typedef struct vertex {
-    int x_s, y_s; // discretized s-steps; vertex would be located at matrix entry M[y_s][x_s]
-    struct vertex * parent; // defaults to NULL is no parent
-    float g_score; 
-    float f_score; // used in priority queue
-    // struct vertex * next; // Pointer to the next vertex in linked list implementation of priority queue
+    /* Homotopy class independent properties of each vertex */
+    int x_s, y_s;                       // discretized s-steps; vertex would be located at matrix entry M[y_s][x_s]
     struct minheap_node * minheap_node; // keep track of corresponding node in min_heap (zero if not in minheap)
-    bool is_evaluated; // keeps track of whether vertex added to closed set or not
+    bool is_evaluated;                  // keeps track of whether vertex added to closed set or not
 
-    /* Homotopy addition */
-    Lval_list_t * L_values;    // The list of Lvalues for the vertex
+    /* Homotopy class relative properties, kept at the vertex level here to reflect the minimum across all homotopy classes */
+    struct vertex * parent; 
+    float g_score; 
+    float f_score;
+
+    /* Linked list containing all homotopic classes through which the vertex is reachable, with relative properties */
+    hom_classes_list_t * hom_classes;    
 
 } vertex_t;
 
